80b7b2c5498c753496357bf61b8e503a9b6b7956
SpengF <27324249@qq.com>
2019-08-22 20:12:00 +0800

    更改注释位置

--- server/app.js
+++ server/app.js
@@ -12,13 +12,10 @@ app.use(router.routes())
 router.post('/index',async ctx=>{
   ctx.body={msg:'我是首页'}
 })
-/**
- * @router 所有用户信息
- * @desc /api/
- * @access 公开
- */
+
 const user=require('./routers/api/user')
 router.use('/api',user)
+
 const port=process.env.PORT || 5000;
 app.listen(port,()=>{
   console.log(`服务已经开启，端口号为${port}`)
--- server/routers/api/user.js
+++ server/routers/api/user.js
@@ -1,5 +1,10 @@
 const Router=require('koa-router')
 const router=new Router();
+/**
+ * @router 所有用户信息
+ * @desc /api/user/login
+ * @access 公开
+ */
 router.get('/user/login',async ctx=>{
   ctx.body={msg:'我是login'}
 })
