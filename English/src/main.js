1bb5eb64fbcc4cbcfc2b149c3317bc614825369d
280431 <27324249@qq.com>
2019-08-22 17:31:53 +0800

    增加功能

--- English/src/main.js
+++ English/src/main.js
@@ -1,14 +1,20 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import elment from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css';
+import mint from 'mint-ui'
 import {fetchPost} from './api/index'
+/**
+ * @compunents 各类组件
+ * @description 注册各类全局组件
+ */
 import {Header} from './components/component'
 Vue.use(Header)
 Vue.config.productionTip = false
 Vue.prototype.$post=fetchPost;
-Vue.use(elment) 
+
+import 'mint-ui/lib/style.css'
+Vue.use(mint) 
+
 import './mock/index.js'
 new Vue({
   el: '#app',
